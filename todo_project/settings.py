aa20f9d009cbe7a273dfe5245c71daefa79dbaba
yyashh03 <singhyashveer765@gmail.com>
2026-04-10 10:09:33 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wxm&#fv4@yvc7-$o4kh%ih)2v%)1yedvb&4psba9ni2$ixst1a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -114,5 +114,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
